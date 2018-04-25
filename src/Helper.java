0baaf500c1af0a4098e312d6e59b13e3ff803ff6
Rishabh Khanna <rishabhkhanna777@gmail.com>
2018-04-24 20:27:48 -0400

    initial commit
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

---
+++ src/Helper.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+
+/**
+ * Created by rishabhkhanna on 20/03/18.
+ */
+public class Helper {
+    public static void printLog(String log){
+        System.out.println(log);
+    }
+}
